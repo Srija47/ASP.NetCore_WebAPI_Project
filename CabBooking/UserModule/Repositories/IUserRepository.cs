e679ae003aa16036e2b83ede79d3f863c6ce82d0
Srija47 <srija.arigela47@gmail.com>
2020-08-03 13:29:40 +0530

    CFApproach

---
+++ CabBooking/UserModule/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserModule.Models;
+
+namespace UserModule.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<bool> UserRegister(User users);
+        Task<Login> UserLogin(Login login);
+        Task<User> GetProfile(int userid);
+        Task<bool> UpdateProfile(User users);
+        Task<bool> DeleteProfile(int userid);
+    }
+}
---
+++ CabBooking/UserModule/Repositories/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using UserModule.Models;
+
+namespace UserModule.Repositories
+{
+    public class UserRepository:IUserRepository
+    {
+        private readonly UserContext _context;
+        public UserRepository(UserContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> UserRegister(User users)
+        {
+
+            _context.Users.Add(users);
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+        public async Task<Login> UserLogin(Login login)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(e => e.Name == login.Name && e.Password == login.Password);
+            if (user != null)
+            {
+                return new Login
+                {
+                    Name = user.Name,
+                    Password = user.Password,
+                    Id = user.Id,
+                };
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+                return null;
+            }
+        }
+        public async Task<User> GetProfile(int userid)
+        {
+            var users = await _context.Users.FindAsync(userid);
+            if (users == null)
+                return null;
+            else
+            {
+                return users;
+            }
+        }
+        public async Task<bool> UpdateProfile(User users)
+        {
+            _context.Users.Update(users);
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+        public async Task<bool> DeleteProfile(int userid)
+        {
+            var users = await _context.Users.FindAsync(userid);
+            if (users != null)
+            {
+                _context.Remove(users);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
